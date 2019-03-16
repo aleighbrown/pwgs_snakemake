ea3260931c2be4d3ac21e8dd615897ae6e6625ae
aleighgreen <brown.annaleigh@gmail.com>
2019-03-16 14:45:22 +0000

    chain adjust2

--- helpers.py
+++ helpers.py
@@ -66,7 +66,7 @@ def makeVCFtypesParam(wildcard):
 #this function generates a string of random numbers given the number of chains we want to run wtih
 
 def make_chains(n_chains):
-    chain_string = "-r " 
+    chain_string = "-r" 
     for i in range(0,n_chains):
         chain_string += " " + str(random.randint(1,10000))
     return(chain_string)
