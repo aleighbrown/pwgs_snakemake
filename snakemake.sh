df3231de36d04d5c840a7d6be45b347ea49daca8
aleighgreen <brown.annaleigh@gmail.com>
2019-05-13 13:32:57 +0100

    getting rid of old sample files

--- snakemake.sh
+++ snakemake.sh
@@ -7,5 +7,5 @@ sbcmd+=" --time=14:00:00 --partition={cluster.partition}"
 sbcmd+=" {cluster.extra}"
 
 snakemake -s run_full_pipeline.snakefile -pr --keep-going --local-cores $SLURM_CPUS_PER_TASK \
-    --jobs 10 --nolock --cluster-config cluster.yaml --cluster "$sbcmd" \
+    --jobs 30 --nolock --cluster-config cluster.yaml --cluster "$sbcmd" \
     --latency-wait 120 all
